7743491746884c10ff8ac63fa55f395905562338
inwork <simpleHelpD@yandex.ru>
2019-01-30 16:03:00 +1000

    Добавил в отрисовку метки длину хвоста для более точного расчета кординат метки

--- Кексобукинг/js/get-pin.js
+++ Кексобукинг/js/get-pin.js
@@ -3,13 +3,14 @@
 (function () {
   var mapPinTemplate = document.querySelector('template')
   .content.querySelector('.map__pin');
+  var PIN_TAIL = 40;
 
   // Разметка метки объявления
   window.getPin = function (offerMark) {
     var pin = mapPinTemplate.cloneNode(true);
     pin.classList.add('hidden');
     pin.style = 'left:' + offerMark.location.x + 'px;' + 'top:' +
-    offerMark.location.y + 'px';
+    (offerMark.location.y - PIN_TAIL) + 'px';
     pin.firstElementChild.src = offerMark.author.avatar;
     pin.firstElementChild.alt = offerMark.offer.title;
     return pin;
